a9bef2e50d0bc60b751d7d879fc7663d79824c7c
sean <sean@10.148.99.3>
2014-04-17 10:39:40 -0600

    I got the GUI populating when you search from a Social Security Number

--- bstDatabase/src/bstDatabase/PopulateTreeNodes.java
+++ bstDatabase/src/bstDatabase/PopulateTreeNodes.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class PopulateTreeNodes {
 
-	private Tree database;
+	private static Tree database;
 	
 	public PopulateTreeNodes() throws Exception{
 		String[] data = Utilities.fileToStringArray("datacitystate.csv");
@@ -12,7 +12,7 @@ public class PopulateTreeNodes {
 		for (int i = 0; i < data.length; i++) {
 			accounts[i] = new Account(data[i]);
 		}
-		Tree database = new Tree(accounts[0]);
+		database = new Tree(accounts[0]);
 		for (int i = 1; i <= accounts.length - 1; i++) {
 			// System.out.println("inserting \"" + accounts[i].toString()+"\"");
 			database.insertNode(database.root, accounts[i]);
@@ -25,37 +25,39 @@ public class PopulateTreeNodes {
 		System.out.println(database.getNode(111111111).getAccount().toString());
 	}
 	
-	public static void main(String[] args) throws IOException {
-		long startTime = System.currentTimeMillis();
-		String[] data = Utilities.fileToStringArray("datacitystate.csv");
-		Account[] accounts = new Account[data.length];
-
-		for (int i = 0; i < data.length; i++) {
-			accounts[i] = new Account(data[i]);
-		}
-
-		Tree database = new Tree(accounts[0]);
-		for (int i = 1; i <= accounts.length - 1; i++) {
-			// System.out.println("inserting \"" + accounts[i].toString()+"\"");
-			database.insertNode(database.root, accounts[i]);
-		}
-
-		System.out.println(Utilities.runtime(startTime));
-
-		Account a = new Account(111111111, "Hamm", "Bacon", 4356289513L,
-				"hammbacon@utahcorp.com", "21 N 1260 E", 84720, "password", 3,
-				27, 1992, "Human Resources");
-
-		database.insertNode(database.root,a);
+	public static void main(String[] args) throws Exception {
+//		long startTime = System.currentTimeMillis();
+//		String[] data = Utilities.fileToStringArray("datacitystate.csv");
+//		Account[] accounts = new Account[data.length];
+//
+//		for (int i = 0; i < data.length; i++) {
+//			accounts[i] = new Account(data[i]);
+//		}
+//
+//		Tree database = new Tree(accounts[0]);
+//		for (int i = 1; i <= accounts.length - 1; i++) {
+//			// System.out.println("inserting \"" + accounts[i].toString()+"\"");
+//			database.insertNode(database.root, accounts[i]);
+//		}
+//
+//		System.out.println(Utilities.runtime(startTime));
+//
+//		Account a = new Account(111111111, "Hamm", "Bacon", 4356289513L,
+//				"hammbacon@utahcorp.com", "21 N 1260 E", 84720, "password", 3,
+//				27, 1992, "Human Resources");
+//
+//		database.insertNode(database.root,a);
+//		System.out.println(database.getNode(111111111).getAccount());
+//		
+//		
+//		System.out.println(database.getNode(181242337).getAccount().toString());
+//		database.deactivateNode(181242337);
+//		System.out.println(database.getNode(181242337).getAccount().toString());
+		PopulateTreeNodes main = new PopulateTreeNodes();
 		System.out.println(database.getNode(111111111).getAccount());
-		
-		
-		System.out.println(database.getNode(181242337).getAccount().toString());
-		database.deactivateNode(181242337);
-		System.out.println(database.getNode(181242337).getAccount().toString());
 	}
 	
-	public Account getAccount(int ssn){
+	public static Account getAccount(int ssn){
 		return database.getNode(ssn).getAccount();
 	}
 }
