ba055ce3038f5490e9c43b52753fc695b0236ae9
sean <sean@10.148.99.3>
2014-04-17 01:59:14 -0600

    Trying to get GUI to work...

--- bstDatabase/src/bstDatabase/PopulateTreeNodes.java
+++ bstDatabase/src/bstDatabase/PopulateTreeNodes.java
@@ -3,6 +3,28 @@ package bstDatabase;
 import java.io.IOException;
 
 public class PopulateTreeNodes {
+
+	private Tree database;
+	
+	public PopulateTreeNodes() throws Exception{
+		String[] data = Utilities.fileToStringArray("datacitystate.csv");
+		Account[] accounts = new Account[data.length];
+		for (int i = 0; i < data.length; i++) {
+			accounts[i] = new Account(data[i]);
+		}
+		Tree database = new Tree(accounts[0]);
+		for (int i = 1; i <= accounts.length - 1; i++) {
+			// System.out.println("inserting \"" + accounts[i].toString()+"\"");
+			database.insertNode(database.root, accounts[i]);
+		}
+		Account a = new Account(111111111, "Hamm", "Bacon", 4356289513L,
+				"hammbacon@utahcorp.com", "21 N 1260 E", 84720, "password", 3,
+				27, 1992, "Human Resources");
+
+		database.insertNode(database.root,a);
+		System.out.println(database.getNode(111111111).getAccount().toString());
+	}
+	
 	public static void main(String[] args) throws IOException {
 		long startTime = System.currentTimeMillis();
 		String[] data = Utilities.fileToStringArray("datacitystate.csv");
@@ -25,9 +47,15 @@ public class PopulateTreeNodes {
 				27, 1992, "Human Resources");
 
 		database.insertNode(database.root,a);
+		System.out.println(database.getNode(111111111).getAccount());
+		
 		
 		System.out.println(database.getNode(181242337).getAccount().toString());
 		database.deactivateNode(181242337);
 		System.out.println(database.getNode(181242337).getAccount().toString());
+	}
+	
+	public Account getAccount(int ssn){
+		return database.getNode(ssn).getAccount();
 	}
 }
--- bstDatabase/src/bstDatabase/bstFrame.java
+++ bstDatabase/src/bstDatabase/bstFrame.java
@@ -13,13 +13,14 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.UIManager;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class bstFrame extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField ssnField;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
@@ -30,24 +31,20 @@ public class bstFrame extends JFrame {
 	private JTextField textField_8;
 	private JTextField textField_9;
 	private JTextField txtYear;
+	private static PopulateTreeNodes main;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws Exception {
+		main = new PopulateTreeNodes();
+		UIManager
+				.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				try {
-					bstFrame frame = new bstFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				bstFrame frame = new bstFrame();
+				frame.setVisible(true);
 			}
 		});
 	}
@@ -66,133 +63,146 @@ public class bstFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Social Security Number");
 		lblNewLabel.setBounds(35, 14, 113, 14);
 		contentPane.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setBounds(158, 11, 127, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
+
+		ssnField = new JTextField();
+		ssnField.setBounds(158, 11, 127, 20);
+		contentPane.add(ssnField);
+		ssnField.setColumns(10);
+
 		JLabel lblFirstName = new JLabel("First Name");
 		lblFirstName.setBounds(10, 48, 62, 14);
 		contentPane.add(lblFirstName);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(77, 45, 106, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblLastName = new JLabel("Last Name");
 		lblLastName.setBounds(193, 48, 62, 14);
 		contentPane.add(lblLastName);
-		
+
 		textField_2 = new JTextField();
 		textField_2.setBounds(265, 45, 113, 20);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Phone");
 		lblNewLabel_1.setBounds(10, 73, 62, 14);
 		contentPane.add(lblNewLabel_1);
-		
+
 		textField_3 = new JTextField();
 		textField_3.setBounds(77, 70, 106, 20);
 		contentPane.add(textField_3);
 		textField_3.setColumns(10);
-		
+
 		JLabel lblEmail = new JLabel("Email");
 		lblEmail.setBounds(193, 73, 46, 14);
 		contentPane.add(lblEmail);
-		
+
 		textField_4 = new JTextField();
 		textField_4.setBounds(227, 70, 151, 20);
 		contentPane.add(textField_4);
 		textField_4.setColumns(10);
-		
+
 		JLabel lblAddress = new JLabel("Address");
 		lblAddress.setBounds(10, 98, 46, 14);
 		contentPane.add(lblAddress);
-		
+
 		textField_5 = new JTextField();
 		textField_5.setBounds(77, 95, 151, 20);
 		contentPane.add(textField_5);
 		textField_5.setColumns(10);
-		
+
 		JLabel lblZip = new JLabel("Zip");
 		lblZip.setBounds(237, 98, 46, 14);
 		contentPane.add(lblZip);
-		
+
 		textField_6 = new JTextField();
 		textField_6.setBounds(265, 95, 113, 20);
 		contentPane.add(textField_6);
 		textField_6.setColumns(10);
-		
+
 		JLabel lblCity = new JLabel("City");
 		lblCity.setBounds(10, 123, 46, 14);
 		contentPane.add(lblCity);
-		
+
 		textField_7 = new JTextField();
 		textField_7.setBounds(77, 120, 151, 20);
 		contentPane.add(textField_7);
 		textField_7.setColumns(10);
-		
+
 		JLabel lblState = new JLabel("State");
 		lblState.setBounds(237, 123, 46, 14);
 		contentPane.add(lblState);
-		
+
 		textField_8 = new JTextField();
 		textField_8.setBounds(265, 120, 113, 20);
 		contentPane.add(textField_8);
 		textField_8.setColumns(10);
-		
+
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setBounds(10, 204, 46, 14);
 		contentPane.add(lblPassword);
-		
+
 		textField_9 = new JTextField();
 		textField_9.setBounds(77, 201, 151, 20);
 		contentPane.add(textField_9);
 		textField_9.setColumns(10);
-		
+
 		JLabel lblBirthday = new JLabel("Birthday");
 		lblBirthday.setBounds(10, 148, 46, 14);
 		contentPane.add(lblBirthday);
-		
+
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Month...", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] { "Month...",
+				"January", "February", "March", "April", "May", "June", "July",
+				"August", "September", "October", "November", "December" }));
 		comboBox.setBounds(77, 145, 101, 20);
 		contentPane.add(comboBox);
-		
+
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Day...", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] { "Day...",
+				"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
+				"13", "14", "15", "16", "17", "18", "19", "20", "21", "22",
+				"23", "24", "25", "26", "27", "28", "29", "30", "31" }));
 		comboBox_1.setBounds(193, 145, 62, 20);
 		contentPane.add(comboBox_1);
-		
+
 		txtYear = new JTextField();
 		txtYear.setText("Year...");
 		txtYear.setBounds(265, 145, 86, 20);
 		contentPane.add(txtYear);
 		txtYear.setColumns(10);
-		
+
 		JLabel lblDepartment = new JLabel("Department");
 		lblDepartment.setBounds(10, 173, 62, 14);
 		contentPane.add(lblDepartment);
-		
+
 		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Sales and Marketing", "Quality Assurance", "Payroll", "Customer Service", "Media Relations", "Customer Relations", "Advertising", "Finances", "Human Resources", "Accounting", "Research and Development", "Tech Support", "Legal Department", "Public Relations", "Asset Management"}));
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {
+				"Sales and Marketing", "Quality Assurance", "Payroll",
+				"Customer Service", "Media Relations", "Customer Relations",
+				"Advertising", "Finances", "Human Resources", "Accounting",
+				"Research and Development", "Tech Support", "Legal Department",
+				"Public Relations", "Asset Management" }));
 		comboBox_2.setBounds(77, 170, 178, 20);
 		contentPane.add(comboBox_2);
-		
+
 		JCheckBox chckbxActive = new JCheckBox("Active");
 		chckbxActive.setBounds(265, 169, 97, 23);
 		contentPane.add(chckbxActive);
-		
+
 		JButton btnNewButton = new JButton("Search");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				int ssn = Integer.parseInt(ssnField.getText());
+				Account account = main.getAccount(ssn); //NullPointerException here.... Not sure why.
+				txtYear.setText(account.getByear()+"");
 			}
 		});
 		btnNewButton.setBounds(298, 10, 80, 23);
